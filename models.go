afdef88358ab3a65149ae4573a20cec5700bd127
James Kokou GAGLO <freemanpolys@gmail.com>
2022-09-01 07:47:17 +0200

    feat: adding subscription model

--- models.go
+++ models.go
@@ -3,13 +3,25 @@ package billing
 import (
 	utils "github.com/andiwork/gorm-utils"
 )
+type SubscriptionStatus string
 
+const (
+	REDIRECT  SubscriptionStatus = "REDIRECT"
+	SUBSCRIBE SubscriptionStatus = "SUBSCRIBE"
+)
 type BillingTransaction struct {
 	utils.Model
 	OrderID           string
 	OrderPaymentToken string
 	AppTrxId          string
 }
+
+type SubscriptionResponse struct {
+	Data interface{}
+	RedirectUrl string
+	Status      SubscriptionStatus
+}
+
 
 // Migrate return models for gorm
 func MigrateModels() (models []interface{}) {
