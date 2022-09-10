4e3d1c6fbc83fb4b5357d13841d472fa97dfd19d
James Kokou GAGLO <freemanpolys@gmail.com>
2022-09-10 08:29:44 +0200

    feat: adding feature

--- repositories.go
+++ repositories.go
@@ -15,3 +15,12 @@ func (r BillingRepository) GetBillingOrderByPaymentToken(paymentToken string, db
 	err = db.First(&model, "order_payment_token = ?", paymentToken).Error
 	return
 }
+
+func (r BillingRepository) GetBillingOrderByTransactionId(transactionID string, db *gorm.DB) (model BillingTransaction, err error) {
+	err = db.First(&model, "app_trx_id = ?", transactionID).Error
+	return
+}
+func (r BillingRepository) GetBillingOrderByPaymentTokenAndTable(table string, paymentToken string, db *gorm.DB) (model BillingTransaction, err error) {
+	err = db.Joins("INNER JOIN ? t ON t.token = billing_transactions.app_trx_id").First(&model, "order_payment_token = ?", paymentToken).Error
+	return
+}
