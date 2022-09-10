46c84e68774f917c648659085f64825ba942f1b3
James Kokou GAGLO <freemanpolys@gmail.com>
2022-09-10 06:09:04 +0200

    feat: add countryAlpha code

--- models.go
+++ models.go
@@ -3,25 +3,27 @@ package billing
 import (
 	utils "github.com/andiwork/gorm-utils"
 )
+
 type SubscriptionStatus string
 
 const (
 	REDIRECT  SubscriptionStatus = "REDIRECT"
 	SUBSCRIBE SubscriptionStatus = "SUBSCRIBE"
 )
+
 type BillingTransaction struct {
 	utils.Model
 	OrderID           string
 	OrderPaymentToken string
 	AppTrxId          string
+	CountryAlpha3     string `gorm:"default:SEN"`
 }
 
 type SubscriptionResponse struct {
-	Data interface{}
+	Data        interface{}
 	RedirectUrl string
 	Status      SubscriptionStatus
 }
-
 
 // Migrate return models for gorm
 func MigrateModels() (models []interface{}) {
